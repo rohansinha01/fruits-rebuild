993759a280fd670d75ed86bd03f5e36b5c8b9e70
rohansinha01 <rohansinha01@gmail.com>
2024-01-02 21:52:16 -0500

    can edit and update

--- server.js
+++ server.js
@@ -126,6 +126,39 @@ app.post("/fruits", async (req, res) => {
     }
 })
 
+// Edit Route (Get to /fruits/:id/edit)
+app.get("/fruits/:id/edit", async (req, res) => {
+    try {
+      // get the id from params
+      const id = req.params.id;
+      // get the fruit from the db
+      const fruit = await Fruit.findById(id);
+      //render the template
+      res.render("fruits/edit.ejs", { fruit });
+    } catch (error) {
+      console.log("-----", error.message, "------");
+      res.status(400).send("error, read logs for details");
+    }
+  });
+
+// Update Route (Put to /fruits/:id)
+app.put("/fruits/:id", async (req,res) => {
+    try {
+        // get teh id 
+        const id = req.params.id
+        // update to ready to eat in req.body
+        req.body.readyToEat = req.body.readyToEat === "on" ? true : false
+        // update the fruit in the database
+        await Fruit.findByIdAndUpdate(id, req.body)
+        // res.redirect back to show page
+        res.redirect(`/fruits/${id}`)
+    } catch (error) {
+    console.log("-----", error.message, "------");
+    res.status(400).send("error, read logs for details");
+    }
+})
+
+
 // Show Route (Get to /fruits/:id)
 app.get("/fruits/:id", async (req, res) => {
     try {
